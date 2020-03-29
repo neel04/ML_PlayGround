71d1ec8ecffe80977aa879c7737d87ec91e313ab
neel04 <neelgupta04@outlook.com>
2020-03-29 16:25:50 +0530

    Create Runner.py
    Run the whole damn project :)

---
+++ Projects/Image_Captioner/Runner.py
@@ -0,0 +1,63 @@
+
+from pickle import load
+from numpy import argmax
+from keras.preprocessing.sequence import pad_sequences
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+from keras.models import load_model
+
+# extract features from each photo in the directory
+def extract_features(filename):
+	# load the model
+	model = VGG16()
+	model.layers.pop()
+	model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+	# load the photo
+	image = load_img(filename, target_size=(224, 224))
+	# convert the image pixels to a numpy array
+	image = img_to_array(image)
+	# reshape data for the model
+	image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+	# prepare the image for the VGG model
+	image = preprocess_input(image)
+	# get features
+	feature = model.predict(image, verbose=0)
+	return feature
+
+# map an integer to a word
+def word_for_id(integer, tokenizer):
+	for word, index in tokenizer.word_index.items():
+		if index == integer:
+			return word
+	return None
+
+# generate a description for an image
+def generate_desc(model, tokenizer, photo, max_length):
+	in_text = 'startseq'
+	for i in range(max_length):
+		# integer encode input sequence
+		sequence = tokenizer.texts_to_sequences([in_text])[0]
+		# pad input
+		sequence = pad_sequences([sequence], maxlen=max_length)
+		yhat = model.predict([photo,sequence], verbose=0)
+		yhat = argmax(yhat)
+		word = word_for_id(yhat, tokenizer)
+		if word is None:
+			break
+		# append as input for generating the next word
+		in_text += ' ' + word
+		if word == 'endseq':
+			break
+	return in_text
+
+# load the tokenizer
+tokenizer = load(open('tokenizer.pkl', 'rb'))
+max_length = 34
+# load the model
+model = load_model('model-ep002-loss3.245-val_loss3.612.h5')
+photo = extract_features('example.jpg')
+description = generate_desc(model, tokenizer, photo, max_length)
+print(description)
